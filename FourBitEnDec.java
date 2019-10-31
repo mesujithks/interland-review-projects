916f0fa62af99f4e71a8763c49347364003ed2e7
Sujith K S <mesujithks3@gmail.com>
2019-10-31 14:03:24 +0530

    Update FourBitEnDec.java

--- FourBitEnDec.java
+++ FourBitEnDec.java
@@ -86,19 +86,19 @@ class FourBitEnDec {
     char toChar(int val){
 	    char ch = '0';
 	    switch(val){
-		    case 0:ch='0';break; 
-            case 1:ch='1';break;
-		    case 2:ch='2';break; 
-            case 3 :ch='3';break;
-		    case 4:ch='4';break; 
-            case 5 :ch='5';break;
-		    case 6:ch='6';break; 
-            case 7 :ch='7';break;
-		    case 8:ch='8';break; 
-            case 9 :ch='9';break;
-            default:ch='0';
-        }
-	    return ch;
+		case 0:ch='0';break; 
+            	case 1:ch='1';break;
+		case 2:ch='2';break; 
+            	case 3 :ch='3';break;
+		case 4:ch='4';break; 
+            	case 5 :ch='5';break;
+		case 6:ch='6';break; 
+            	case 7 :ch='7';break;
+		case 8:ch='8';break; 
+            	case 9 :ch='9';break;
+            	default:ch='0';
+       	}
+	return ch;
     }
     
 }
